b7b143904fa06a595d152c3dee9e0c898eb0a2c7
gauravrajput9 <149414614+gauravrajput9@users.noreply.github.com>
2025-07-16 21:27:02 +0530

    Update courseApi.js

--- frontend/src/api/courseApi.js
+++ frontend/src/api/courseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const app = axios.create({
-  baseURL: "http://localhost:3000/course",
+  baseURL: `${process.env.REACT_APP_CLIENT_URL}/course`,
   withCredentials: true,
 });
 export const createCourse = async (formData) => {
