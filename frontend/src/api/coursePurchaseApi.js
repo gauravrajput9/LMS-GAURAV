81d03e9b4ac8799113cee9a422f5799c1ef2807a
r1a2j3p4u5t <rajputgaurav8135@gmail.com>
2025-07-17 15:00:46 +0530

    fix api bugs

--- frontend/src/api/coursePurchaseApi.js
+++ frontend/src/api/coursePurchaseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const app = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + '/progress',
+    baseURL: import.meta.env.VITE_API_URL + '/purchase',
     withCredentials: true
 })
 
