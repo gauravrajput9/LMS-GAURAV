e5e5532542304e05de00afc39c20a632b20fbcfb
gauravrajput9 <149414614+gauravrajput9@users.noreply.github.com>
2025-07-17 14:05:14 +0530

    Update coursePurchaseApi.js

--- frontend/src/api/coursePurchaseApi.js
+++ frontend/src/api/coursePurchaseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const app = axios.create({
-    baseURL: `${process.env.REACT_APP_CLIENT_URL}/progress`,
+    baseURL: `${process.env.REACT_APP_API_BASE_URL}/progress`,
     withCredentials: true
 })
 
